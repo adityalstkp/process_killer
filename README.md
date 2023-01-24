e0b265f4428fd34860badedf9fe31c8440c50315
Aditya Septiadi <aditya.septiadi@tokopedia.com>
2023-01-24 09:46:13 +0700

    docs: add todo

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Process killer based on config JSON file.
 
 TODO:
 1. Parse expired with `h` and `m` unit.
-2. Kill procs when expired
+2. Parse args when running binary.
+3. Kill procs when expired
--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,15 @@ use sysinfo::{System, SystemExt, ProcessExt};
 
 // Register procs killer by procs name
 fn reg_procs_killer(sys: &mut System, procs_name: &str) {
-    for procs in sys.processes_by_name(procs_name) {
+    for procs in sys.processes_by_exact_name(procs_name) {
         println!("name: {} | pid: {}", procs.name(), procs.pid())
     }
 }
 
-
-
 fn main() {
     let mut sys = System::new();
     let cfg_str = fs::read_to_string("./test_files/config.json").expect("config must exist");
-    let procs_cfg = procs_cfg::parse_config(&cfg_str).expect("not valid config");
+    let procs_cfg = procs_cfg::parse_config(&cfg_str).expect("must have a valid config");
 
     loop {
         sys.refresh_processes();
